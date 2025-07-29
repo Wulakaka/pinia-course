04493860f66f68663621408f3bdbec955d764a71
zhangweifeng <zhang.weifeng@tslsmart.com>
2025-07-29 22:32:42 +0800

    feat: 更新 vite 版本，添加 store

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-const path = require('path')
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueDevTools from "vite-plugin-vue-devtools";
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src')
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [vue()]
-})
+  plugins: [VueDevTools(), vue()],
+});
