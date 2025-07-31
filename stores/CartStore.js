09b8e72dbc01c85a1289c82b88c4fc7b0e8c3d29
zhangweifeng <zhang.weifeng@tslsmart.com>
2025-07-31 22:52:16 +0800

    feat: 完善逻辑

---
+++ stores/CartStore.js
@@ -0,0 +1,19 @@
+import {defineStore} from "pinia";
+
+export const useCartStore = defineStore("CartStore", {
+  state: () => ({
+    items: [],
+  }),
+  getters: {
+    count: (state) => state.items.length,
+    isEmpty: (state) => state.count === 0,
+  },
+  actions: {
+    addItems(count, item) {
+      count = parseInt(count);
+      for (let index = 0; index < count; index++) {
+        this.items.push({...item});
+      }
+    },
+  },
+});
